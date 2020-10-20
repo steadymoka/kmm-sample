232aed07a6a0afdcd5bee0e14d7264c066f43ee4
moka-a <aud1220a@gmail.com>
2020-10-21 00:12:37 +0900

    wip

--- iosApp/iosApp/ui/home/HomeView.swift
+++ iosApp/iosApp/ui/home/HomeView.swift
@@ -1,8 +1,27 @@
 import SwiftUI
+import shared
 
 struct HomeView: View {
     var body: some View {
         Text("Hello, World!\nHome")
+                .onAppear {
+                    aaaa()
+                }
+    }
+
+    func observe( _ action: () -> Unit) {
+
+    }
+    
+    func aaaa() {
+        let container = AppContainer()
+        ProfileScope().onCreate(container: container)
+        let viewModel = container.profileContainer!.viewModel
+
+        viewModel.profile.observe { (value) in
+            debugPrint("aaaaaaaaaaaaa value: \(value)")
+        }
+        viewModel.loadProfileData { unit, error in }
     }
 }
 
@@ -10,4 +29,12 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
+}
+
+extension Kotlinx_coroutines_coreFlow {
+
+    func observe( _ action: @escaping (Any?) -> Void) {
+        ApiUtilKt.observe(self, action: action)
+    }
+
 }
