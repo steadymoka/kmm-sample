ea5d93f35e61a2463c18191986de045dc61a5b79
moka-a <aud1220a@gmail.com>
2020-10-23 14:24:04 +0900

    improve preference

--- shared/src/commonMain/kotlin/land/moka/kmm/shared/di/module/DatabaseModule.kt
+++ shared/src/commonMain/kotlin/land/moka/kmm/shared/di/module/DatabaseModule.kt
@@ -1,7 +1,12 @@
 package land.moka.kmm.shared.di.module
 
+import land.moka.kmm.shared.lib.preferecne.Preference
 import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
 
 val databaseModule = DI.Module("database") {
-
+    bind<Preference>() with singleton {
+        Preference()
+    }
 }
---
+++ shared/src/iosMain/kotlin/land/moka/kmm/shared/lib/preferecne/Preference.kt
@@ -0,0 +1,121 @@
+package land.moka.kmm.shared.lib.preferecne
+
+import platform.Foundation.NSUserDefaults
+
+actual class Preference {
+
+    private val userDefault: NSUserDefaults
+
+    actual constructor(name: String?) {
+        userDefault = NSUserDefaults(suiteName = name)
+    }
+
+    actual fun setInt(key: String, value: Int) {
+        return userDefault.setInteger(value.toLong(), key)
+    }
+
+    actual fun getInt(key: String, defaultValue: Int): Int {
+        return if (hasKey(key)) {
+            userDefault.integerForKey(key).toInt()
+        } else {
+            defaultValue
+        }
+    }
+
+    actual fun getInt(key: String): Int? {
+        return if (hasKey(key)) {
+            userDefault.integerForKey(key).toInt()
+        } else {
+            null
+        }
+    }
+
+    actual fun setFloat(key: String, value: Float) {
+        return userDefault.setFloat(value, key)
+    }
+
+    actual fun getFloat(key: String, defaultValue: Float): Float {
+        return if (hasKey(key)) {
+            userDefault.floatForKey(key)
+        } else {
+            defaultValue
+        }
+    }
+
+    actual fun getFloat(key: String): Float? {
+        return if (hasKey(key)) {
+            userDefault.floatForKey(key)
+        } else {
+            null
+        }
+    }
+
+    actual fun setLong(key: String, value: Long) {
+        return userDefault.setInteger(value, key)
+    }
+
+    actual fun getLong(key: String, defaultValue: Long): Long {
+        return if (hasKey(key)) {
+            userDefault.integerForKey(key)
+        } else {
+            defaultValue
+        }
+    }
+
+    actual fun getLong(key: String): Long? {
+        return if (hasKey(key)) {
+            userDefault.integerForKey(key)
+        } else {
+            null
+        }
+    }
+
+    actual fun setString(key: String, value: String) {
+        return userDefault.setObject(value, key)
+    }
+
+    actual fun getString(key: String, defaultValue: String): String {
+        return userDefault.stringForKey(key) ?: defaultValue
+    }
+
+    actual fun getString(key: String): String? {
+        return if (hasKey(key)) {
+            userDefault.stringForKey(key) ?: ""
+        } else {
+            null
+        }
+    }
+
+    actual fun setBoolean(key: String, value: Boolean) {
+        return userDefault.setBool(value, key)
+    }
+
+    actual fun getBoolean(key: String, defaultValue: Boolean): Boolean {
+        return if (hasKey(key)) {
+            userDefault.boolForKey(key)
+        } else {
+            defaultValue
+        }
+    }
+
+    actual fun getBoolean(key: String): Boolean? {
+        return if (hasKey(key)) {
+            userDefault.boolForKey(key)
+        } else {
+            null
+        }
+    }
+
+    actual fun hasKey(key: String): Boolean = userDefault.objectForKey(key) != null
+
+    actual fun clear() {
+        for (key in userDefault.dictionaryRepresentation().keys) {
+            remove(key as String)
+        }
+    }
+
+    actual fun remove(key: String) {
+        userDefault.removeObjectForKey(key)
+    }
+
+}
